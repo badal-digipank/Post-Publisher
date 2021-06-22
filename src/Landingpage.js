b0064e1906cf168b04eef1c2b09f3e7705d3216b
Badal <badal@digipank.com>
2021-06-22 15:32:56 +0530

    post publisher

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -34,9 +34,100 @@ function Landingpage() {
                  Choose the best plan that fit best for you
                  </div>
                  <div className="chooset">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+                 <div className="plangrp">
+                     
+                 </div>
+                 <div className="B" >
+                 <div className="basic">Basic</div>
+                <div className="bdetail">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+       <button className="bbtn"><span className="in1">Get Started</span> </button>
+       <div className="one">01</div>
+       <div className="l1"></div>
+       <div className="p1">$5</div>
+       </div>
+
+       <div className="standard">Standard</div>
+                <div className="sdetail">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+       <button className="sbtn"><span className="in2">Get Started</span> </button>
+       <div className="two">02</div>
+       <div className="l2"></div>
+       <div className="p2">$15</div>
+       
+
+       <div className="professional">Professional</div>
+                <div className="pdetail">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+       <button className="pbtn"><span className="in3">Get Started</span> </button>
+       <div className="three">03</div>
+       <div className="l3"></div>
+       <div className="p3">$25</div>
+       <div className="Group44"><img src="mam.png" alt="lif"/></div>
+       <div className="easier">Learn to makes finding, scheduling, managing, and reporting on social media content easier</div>
+        <div className="easier1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+        <div className="h1">Learn to makes finding, scheduling, managing, and reporting on social media content easier</div>
+        <div className="h2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+        <div className="mask"><img src="Mask Group.svg" alt="mask"/></div>
+        <div className="v7"><img src="Vector 7.png" alt="vector"/></div>
+        <div className="group"><img src="Group.png" alt="group"/></div>
+        
+        <div className="arrow"><img src="arrow.png" alt="arrow"/></div>
+        <div className="blue"></div>
+        <div className="blue1">The best way to manage social media account</div>
+        <div className="blue2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+        <button className="bluebtn"><span className="bluebtni">Get Started</span></button>
+        <div className="pb"><img src="Patten1.png" alt="pbottom"/></div> 
+        <div className="pt"><img src="Patten.png" alt="ptop"/></div>
+        <div className="read">Read our latest story</div>
+        <div className="mobilebox"></div>
+       <div className="img1"><img src="mobiles.png" alt="mobile"/></div>
+       <div className="dronebox"></div>
+       <div className="speakerbox"></div>
+         <div className="img2"><img src="drone.png" alt="drone"/></div>
+        <div className="img3"><img src="speaker.png" alt="speaker"/></div>
+        <div className="mbt1">Lorem Ipsum is text</div>
+        <div className="dbt1">Lorem Ipsum is text</div>
+        <div className="sbt1">Lorem Ipsum is text</div>
+        <div className="mbt2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+         <div className="dbt2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+         <div className="sbt2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+         {/* <div className="rbox">  </div> */}
+         <div className="mr">Read more </div>
+         <div className="sym1"><img src="moresy.png" alt="next"/></div>
+       
+         <div className="dr">Read more </div>
+        <div className="sym2"><img src="moresy.png" alt="next1"/></div>
+
+         <div className="sr">Read more </div>
+         <div className="sym3"><img src="moresy.png" alt="next2"/></div>
+        <div className="get">Get touch with us</div>
+         <div className="g"><img src="Group 1421.png" alt="group"/></div>
+         
+         
+         
+         <input className="yourname" type="text" placeholder="Your name"></input>
+         <input className="email" type="text" placeholder="Email"></input>
+         <textarea className="thoughts" type="text" placeholder="Share your thoughts">
+             
+         </textarea>
+         <div className="footer"></div>
+         <div className="pimg"><img src="Group 1373.png" alt="p"/></div>
+         <div className="downp" >Postpublisher</div>
+         <div className="dtext">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+        <div className="line"></div>
+
+        <div className="last">Copyright © 2021 postpublisher</div>
+        <div className="downleft">Postpublisher</div>
+        <div className="about">About us</div>
+        <div className="contact">Contact us</div>
+        <div className="market">Marketing videos</div>
+        <div className="free">Free consultation</div>
+        <div className="legal">Legal</div>
+        <div className="privacy">Privacy policy</div>
+        <div className="terms">Terms of use</div>
+        <div className="lpng"><img src="Group 1376.png" alt="fit"/></div>
         </div>
-               
+                
     )
+    
 }
 
 export default Landingpage
